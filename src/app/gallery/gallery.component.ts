6fd8bd6dbdf254d998122f3fe3a490a5174fc5d3
robert1106 <robota1106@gmail.com>
2021-06-02 00:12:33 +0300

    finish gallery page

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {TabsetConfig} from "ngx-bootstrap/tabs";
+
+export function getTabsetConfig(): TabsetConfig {
+  return Object.assign(new TabsetConfig(), { type: 'pills', isKeysAllowed: true });
+}
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  providers: [{ provide: TabsetConfig, useFactory: getTabsetConfig }]
 })
 export class GalleryComponent implements OnInit {
 
