4a17b33f6cf10bb8a1436660cfd31256368aa1a1
robert1106 <robota1106@gmail.com>
2021-07-27 12:19:28 +0300

    container margin

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -60,6 +60,95 @@ export class AsideComponent implements OnInit {
       'williamsburg tote bag farm-to-table mustache ugh deep v irony. Af man bun copper mug iPhone enamel pin pug ' +
       'selvage hammock palo santo godard thundercats coloring book yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
       'edison bulb farm-to-table'
+  },{
+    type: 'post',
+    blogMediaPaths: [
+      {
+        image: '../../assets/images/blog/slide-blog.svg',
+      },
+    ],
+    createdAt: 'August 10, 2020',
+    fistAuthor: 'by Ann Summers',
+    category: 'Simple interiror design',
+    title: 'Red humblebrag',
+    info: 'Everyday carry a farm-to-table mustache ugh deep v irony. Af man bun copper mug iPhone enamel pin pug ' +
+      'selvage hammock palo santo godard yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-table'
+  },{
+    type: 'post-video',
+    blogMediaPaths: [
+      {
+        image: '../../assets/images/play_button.svg',
+      },
+    ],
+    createdAt: 'August 12, 2020',
+    fistAuthor: 'by Lux Morningstar',
+    category: 'Design',
+    title: 'Authentic kogi shabby chic'
+  },{
+    type: 'post',
+    blogMediaPaths: [
+      {
+        image: '../../assets/images/blog/slide-blog.svg',
+      },
+    ],
+    createdAt: 'August 11, 2020',
+    fistAuthor: 'by Ann Summers',
+    category: 'Simple interiror design',
+    title: 'Big posts',
+    info: 'Everyday carry a farm-to-table mustache ugh deep v irony. Af man bun copper mug iPhone enamel pin pug ' +
+      'selvage hammock palo santo godard yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-tableEv dard yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-tableEveryday carry a farm-to-table mustache ugh deep v irony. Af man bun copper mug iPhone enamel pin pug ' +
+      'selvage hammock palo santo godard yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-table'
+  },{
+    type: 'post-video',
+    blogMediaPaths: [
+      {
+        image: '../../assets/images/play_button.svg',
+      },
+    ],
+    createdAt: 'August 12, 2020',
+    fistAuthor: 'by Lux Morningstar',
+    category: 'Office furniture',
+    title: 'Authentic kogi shabby chic'
+  },{
+    type: 'post',
+    blogMediaPaths: [
+      {
+        image: '../../assets/images/blog/post1.svg',
+      },{
+        image: '../../assets/images/blog/blog-video.svg',
+      }, {
+        image: '../../assets/images/blog/slide-blog.svg',
+      },
+    ],
+    createdAt: 'August 15, 2020',
+    fistAuthor: 'by Ann Summers',
+    category: 'Office furniture',
+    title: 'Red selfies edison bulb four dollar toast humblebrag for the furniture',
+    info: 'Everyday carry actually neutra authentic kogi shabby chic migas small batch craft beer. Literally ' +
+      'williamsburg tote bag farm-to-table mustache ugh deep v irony. Af man bun copper mug iPhone enamel pin pug ' +
+      'selvage hammock palo santo godard thundercats coloring book yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-table'
+  },{
+    type: 'post',
+    blogMediaPaths: [
+      {
+        image: '../../assets/images/blog/slide-blog.svg',
+      },
+    ],
+    createdAt: 'August 11, 2020',
+    fistAuthor: 'by Ann Summers',
+    category: 'Dining room',
+    title: 'Big posts',
+    info: 'Everyday carry a farm-to-table mustache ugh deep v irony. Af man bun copper mug iPhone enamel pin pug ' +
+      'selvage hammock palo santo godard yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-tableEv dard yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-tableEveryday carry a farm-to-table mustache ugh deep v irony. Af man bun copper mug iPhone enamel pin pug ' +
+      'selvage hammock palo santo godard yuccie woke. Ugh pok pok taxidermy pabst enamel pin ' +
+      'edison bulb farm-to-table'
   },];
 
   ngOnInit(): void {
